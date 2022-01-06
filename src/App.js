a35165b9782294b85b347f6d686ed9f709268aea
Tim Bauer <timbauer@ymail.com>
2022-01-05 22:24:58 -0800

    Section 4 Lesson 23 complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import { v4 as uuidv4} from 'uuid'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -14,11 +15,17 @@ function App() {
     }
   }
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+    console.log(feedback)
+  }
+
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback}/>
         <FeedbackStats feedback={feedback}/>
         <FeedbackList
           feedback={feedback}
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Button from './shared/Button'
 import RatingSelect from './RatingSelect';
 import { useState } from 'react'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [reviewText, setReviewText] = useState('')
   const [rating, setRating] = useState(10)
   const [isSubmitButtonDisabled, setIsSubmitButtonDisabled] = useState(true)
@@ -22,9 +22,23 @@ function FeedbackForm() {
     setReviewText(() => e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(reviewText.trim().length >= 5) {
+        const newFeedback = {
+            text: reviewText,
+            rating
+        }
+
+        handleAdd(newFeedback)
+        setReviewText('')
+        setRating(10)
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us...</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
